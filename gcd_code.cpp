295d096cbf7cd8c455d21e28cbd41ba93eb14196
ANIKAT RANA <83514950+ANIKAT17@users.noreply.github.com>
2022-05-24 02:04:05 +0530

    Update gcd_code.cpp

--- gcd_code.cpp
+++ gcd_code.cpp
@@ -14,3 +14,16 @@ int main()
  gcd(4,8);
  
 }
+// here is another code for the gcd 
+
+#include<bits/stdc++.h>
+using namespace std;
+int gcd (int a, int b) {
+    return  b ? gcd (b, a % b) : a;
+}
+int main()
+{
+
+ cout<<gcd(2,4)<<endl;
+ 
+}

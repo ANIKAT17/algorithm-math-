4ee8bc6de65037572783c0eebbb4b564fbb5b76f
ANIKAT RANA <83514950+ANIKAT17@users.noreply.github.com>
2022-05-24 01:45:21 +0530

    Update 232.cpp

--- 232.cpp
+++ 232.cpp
@@ -1,17 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 long long binpow(long long a, long long b) {
-    long long res = 1;
-    while (b > 0) {
-        if (b & 1)
-            res = res * a;
-        a = a * a;
-        b >>= 1;
-    }
-    cout<<res<<"\n";
-    return res;
-}int main()
+    if (b == 0)
+        return 1;
+    long long res = binpow(a, b / 2);
+    if (b % 2)
+        return res * res * a;
+    else
+        {
+            cout<<res*res<<endl;
+        }
+        return res * res;
+
+
+}
+int main()
 {
- binpow(3,4);
+ binpow(3,2);
  
 }

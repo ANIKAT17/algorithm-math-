87a04d174de30bd2b0869b20a25db3cc8b71bc50
ANIKAT RANA <83514950+ANIKAT17@users.noreply.github.com>
2022-05-24 01:38:01 +0530

    Add files via upload

---
+++ 232.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    cout<<res<<endl;
+    return res;
+}
+int main()
+{
+ binpow(3,4);
+ 
+}
